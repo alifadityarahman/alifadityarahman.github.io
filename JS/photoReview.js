cab12acf38b46a0e158d944227a31b2884cac9d0
Alif Aditya Rahman <182410101075@cs.unej.ac.id>
2019-12-18 14:10:00 +0700

    Extend JQUERY Part Last
    For Photo Reviewer

--- JS/photoReview.js
+++ JS/photoReview.js
@@ -1,10 +1,33 @@
-var inputfoto = document.getElementById('inputfoto');
-inputfoto.addEventListener('change', function (inputfoto) {
-    var box = document.getElementById("box");
-    var img = inputfoto.target.files;
+// PENGGUNAAN JQUERY
+function readURL(input) {
+  if (input.files && input.files[0]) {
     var reader = new FileReader();
     reader.onload = function (e) {
-        box.setAttribute('src', e.target.result);
+      if (e != "") {
+        $('#preview').show();
+        $('#preview').attr('src', e.target.result);
+      } else {
+        $('#preview').show();
+      }
     }
-    reader.readAsDataURL(img[0]);
+    reader.readAsDataURL(input.files[0]);
+  }
+}
+
+$("#gambar_pro").change(function () {
+  readURL(this);
 });
+
+
+// PENGGUNAAN NATIVE
+
+// var inputfoto = document.getElementById('inputfoto');
+// inputfoto.addEventListener('change', function (inputfoto) {
+//   var box = document.getElementById("box");
+//   var img = inputfoto.target.files;
+//   var reader = new FileReader();
+//   reader.onload = function (e) {
+//     box.setAttribute('src', e.target.result);
+//   }
+//   reader.readAsDataURL(img[0]);
+// });
